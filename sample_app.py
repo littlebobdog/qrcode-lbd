d889a6fc037b0fd74a3e4cefb53455d3881c8e70
littlebobdog <sonhikoy@gmail.com>
2024-07-15 15:33:41 +0900

    refactor code

--- sample_app.py
+++ sample_app.py
@@ -2,7 +2,20 @@ import streamlit as st
 import qrcode
 import io
 
-def generate_qr_code(input_string, qr_version, box_size, border, 
+st.set_page_config(
+    page_title="qrcode-lbd",
+    page_icon=":dog:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
+
+if 'history' not in st.session_state:
+    st.session_state.history = []
+
+def generate_qr_code(input_text, qr_version, box_size, border, 
                      fill_color, back_color, error_correction):
     qr = qrcode.QRCode(
         version=qr_version,
@@ -10,44 +23,39 @@ def generate_qr_code(input_string, qr_version, box_size, border,
         box_size=box_size,
         border=border,
     )
-    qr.add_data(input_string)
+    qr.add_data(input_text)
     qr.make(fit=True)
     qr_img = qr.make_image(fill_color=fill_color, back_color=back_color)
     img_byte_arr = io.BytesIO()
     qr_img.save(img_byte_arr, format='PNG')
     return img_byte_arr.getvalue()
 
-@st.experimental_dialog("QR Code Generator")
-def display_qr_code(input_string, qr_version, box_size, border, 
-                    fill_color, back_color, error_correction):
-    if input_string:
-        qr_img_bytes = generate_qr_code(input_string, qr_version, box_size, border, 
-                                        fill_color, back_color, error_correction)
-        st.image(qr_img_bytes, caption=f"input_text: {input_string}", use_column_width=True)
+@st.experimental_dialog("Result")
+def display_qr_code(input_text, qr_version, box_size, border, fill_color, back_color, error_correction):
+    if input_text:
+        qr_img_bytes = generate_qr_code(input_text, qr_version, box_size, border, fill_color, back_color, error_correction)
+        st.image(qr_img_bytes, caption=f"input_text: {input_text}", use_column_width=True)
+        st.success("QR code generated successfully.")
     else:
-        st.warning("Please input text and apply.")
+        st.error("Please input text and apply.")
 
 def main():
     qrcode_url = "more info: https://pypi.org/project/qrcode/"
 
-    st.sidebar.title("menu")
-    color1, color2 = st.sidebar.columns(2)
-    qr_version = st.sidebar.number_input("QR version", value=10, min_value=1, max_value=40, step=1, 
+    with st.sidebar:
+        st.title("Settings")
+        st.info("Config generator settings.Here is default settings.")
+        color1, color2 = st.columns(2)
+        qr_version = st.number_input("QR version", value=10, min_value=1, max_value=40, step=1, 
                                          help=qrcode_url)
-    error_correction_string = st.sidebar.selectbox("Error correction", 
+        error_correction_string = st.selectbox("Error correction", 
                                                    ["ERROR_CORRECT_L", "ERROR_CORRECT_M", 
                                                     "ERROR_CORRECT_Q",  "ERROR_CORRECT_H"], help=qrcode_url)
-    box_size = st.sidebar.number_input("Box size", value=20, min_value=1, step=1, help=qrcode_url)
-    border = st.sidebar.number_input("Border", value=4, min_value=0, step=1, help=qrcode_url)
-    color1, color2 = st.sidebar.columns(2)
-    fill_color = color1.color_picker("Fill color", value="#000000", help=qrcode_url)
-    back_color = color2.color_picker("Back color", value="#FFFFFF", help=qrcode_url)
-
-    st.title("QR code generator")
-    col1, col2 = st.columns([4, 1], vertical_alignment="bottom")
-    input_text = col1.text_input("Input", placeholder="Apply to preview", 
-                                 help=qrcode_url)
-    show_dialog_button = col2.button("show dialog")
+        box_size = st.slider("Box size", value=20, min_value=1, step=1, help=qrcode_url)
+        border = st.slider("Border", value=4, min_value=0, step=1, help=qrcode_url)
+        color1, color2 = st.columns(2)
+        fill_color = color1.color_picker("Fill color", value="#000000", help=qrcode_url)
+        back_color = color2.color_picker("Back color", value="#FFFFFF", help=qrcode_url)
 
     error_correction = qrcode.constants.ERROR_CORRECT_M
     match error_correction_string:
@@ -60,11 +68,26 @@ def main():
         case "ERROR_CORRECT_H":
             error_correction = qrcode.constants.ERROR_CORRECT_H
 
-    if show_dialog_button:
-        display_qr_code(input_text, qr_version, box_size, border, fill_color, back_color, error_correction)
-    elif input_text != "":
+    st.title("QR code generator")
+        
+    generate_tab, history_tab = st.tabs(["Generate", "History"])
+
+    col1, col2 = generate_tab.columns([4, 1], vertical_alignment="bottom")
+    input_text = col1.text_input("Input", placeholder="Input text here", 
+                                 help=qrcode_url)
+    col2.button("popup", on_click=display_qr_code, args=(input_text, qr_version, box_size, border, fill_color, back_color, error_correction))
+
+    if input_text != "":
         qr_img_bytes = generate_qr_code(input_text, qr_version, box_size, border, fill_color, back_color, error_correction)
-        st.image(qr_img_bytes, caption=f"input_text: {input_text}", use_column_width=True)
+        generate_tab.image(qr_img_bytes, caption=f"input_text: {input_text}")
+        generate_tab.success("QR code preview successfully.")
+        st.session_state.history.append(input_text)
+        st.toast("generate success and added to history")
+        st.balloons()
+
+    with history_tab.expander("Session History (Delete by closing the browser)"):
+        for text in st.session_state.history:
+            history_tab.write(text)
 
 if __name__ == "__main__":
     main()
